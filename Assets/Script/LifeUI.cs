4e471908e45055d134a5e7fc14b871236277a32b
佐藤圭 <>
2020-09-11 15:56:00 +0900

    少し改善

--- Assets/Script/LifeUI.cs
+++ Assets/Script/LifeUI.cs
@@ -15,12 +15,6 @@ public class LifeUI : MonoBehaviour
 
     public Text ID;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
